d4d9d897f472691d7334a94d27087c21a8d91a1b
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-24 16:13:16 +0700

    mendapatkan angka dari element di array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -12,6 +12,8 @@ namespace SearchAlgorithm
         int[] arr = new int[20];
         //Number of elements in the array
         int n;
+        //Get the number of elements to store in the array
+        int i;
         static void Main(string[] args)
         {
         }
